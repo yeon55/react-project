fc0ba0e55d625970838f6cf8ff92bec830d9f55b
SEOYEON HAN <tjdus0824@naver.com>
2022-03-12 01:10:50 +0900

    ADD: ExpenseItem.js에 내용 추가

--- react-project/src/components/ExpenseItem.js
+++ react-project/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem(){
-    return <h2>Expense item!</h2>
+    return (
+        <>
+            <div>March 28th 2022</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$294.67</div>
+            </div>
+        </>
+    )
 }
 
 export default ExpenseItem;
