9056cc9862bba380a133c02175b183cee80fb1fe
SEOYEON HAN <tjdus0824@naver.com>
2022-03-15 20:33:50 +0900

    ADD: ExpenseForm.js에 Title,Amount, Date 입력 Form생성하기

--- react-project/src/components/NewExpense/ExpenseForm.js
+++ react-project/src/components/NewExpense/ExpenseForm.js
@@ -8,6 +8,14 @@ const ExpenseForm = () => {
                 <label>Title</label>
                 <input type="text" />
             </div>
+            <div className="new-expense__control">
+                <label>Amount</label>
+                <input type="number" min="0.01" step="0.01"/>
+            </div>
+            <div className="new-expense__control">
+                <label>Date</label>
+                <input type="date" min="2022-01-01" max="2025-12-31" />
+            </div>
         </div>
     </form>
 };
