2cbb048310f3d35ca0915457e2bc6d4a460c76aa
SEOYEON HAN <tjdus0824@naver.com>
2022-03-27 14:22:22 +0900

    ADD: ExpenseForm.js에 여러개의 state 생성하기

--- react-project/src/components/NewExpense/ExpenseForm.js
+++ react-project/src/components/NewExpense/ExpenseForm.js
@@ -1,26 +1,45 @@
-import React from "react";
-import './ExpenseForm.css';
+import React, { useState } from "react";
+import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    return <form>
-        <div className="new-expense__controls">
-            <div className="new-expense__control">
-                <label>Title</label>
-                <input type="text" />
-            </div>
-            <div className="new-expense__control">
-                <label>Amount</label>
-                <input type="number" min="0.01" step="0.01"/>
-            </div>
-            <div className="new-expense__control">
-                <label>Date</label>
-                <input type="date" min="2022-01-01" max="2025-12-31" />
-            </div>
+  const [enteredTitle, setEnteredTitled] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  const titleChangeHandler = (e) => {
+    setEnteredTitled(e.target.value)
+  };
+
+  const amountChangeHandler = (e) => {
+    setEnteredAmount(e.target.value)
+  };
+
+  const dateChangeHandler = (e) =>{
+    setEnteredDate(e.value.target)
+  };
+
+
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler} />
         </div>
-        <div className="new-expense__actions">
-            <button type="submit">Add Expense</button>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
         </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2022-01-01" max="2025-12-31" onChange={dateChangeHandler}/>
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
     </form>
+  );
 };
 
 export default ExpenseForm;
