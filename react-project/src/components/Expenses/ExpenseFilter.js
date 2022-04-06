d0a7db195f8aec673e93afc8338051fa014e7046
SEOYEON HAN <tjdus0824@naver.com>
2022-04-06 18:59:50 +0900

    FIX: 코드 오류 찾아 재작성하기

--- react-project/src/components/Expenses/ExpenseFilter.js
+++ react-project/src/components/Expenses/ExpenseFilter.js
@@ -1,23 +1,25 @@
-import ExpenseForm from '../NewExpense/ExpenseForm';
+import React from 'react';
+
 import './ExpenseFilter.css';
 
-const ExpensesFilter = () => {
-    const dropdownChangeHandler = (e) => {
-        console.log(e.target.value);
-    };
-    return (
-        <div className='expense-filter'>
-            <div className='expense-filter__control'>
-                <label>Filter by year</label>
-                <select value={props.selected} onChange={dropdownChangeHandler}>
-                    <option value='2022'>2022</option>
-                    <option value='2021'>2021</option>
-                    <option value='2019'>2019</option>
-                    <option value='2018'>2018</option>
-                </select>
-            </div>
-        </div>
-    );
+const ExpensesFilter = (props) => {
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
+
+  return (
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
+          <option value='2022'>2022</option>
+          <option value='2021'>2021</option>
+          <option value='2020'>2020</option>
+          <option value='2019'>2019</option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default ExpensesFilter;
