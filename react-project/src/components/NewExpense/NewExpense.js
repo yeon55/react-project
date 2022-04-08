dece942ad4f800950936d65fdde238d268e11dd2
SEOYEON HAN <tjdus0824@naver.com>
2022-04-08 23:59:37 +0900

    ADD: 조건에 따라 form 형태 보여주는 button추가작업

--- react-project/src/components/NewExpense/NewExpense.js
+++ react-project/src/components/NewExpense/NewExpense.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData, 
@@ -11,9 +12,13 @@ const NewExpense = (props) => {
         props.onAddExpense(expenseData);
     };
 
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  }
   return (
       <div className="new-expense">
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+        {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+        {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />}
       </div>
   );
 };
