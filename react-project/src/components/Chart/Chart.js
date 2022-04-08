39d628610065fa5f5e323e121a6121914efc6385
SEOYEON HAN <tjdus0824@naver.com>
2022-04-09 00:46:38 +0900

    ADD: 차트막대기 만들기

--- react-project/src/components/Chart/Chart.js
+++ react-project/src/components/Chart/Chart.js
@@ -3,13 +3,18 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map(
+    (dataPoints) => dataPoints.value
+  );
+  const totalMaximum = Math.max(...dataPointValues);
+
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
